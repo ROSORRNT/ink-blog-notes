1023756b480700b037077a29b441eab37555b6db
ROSORRNT <45917975+ROSORRNT@users.noreply.github.com>
2024-02-19 12:15:40 +0100

    Add new images and data files

---
+++ src/data/data.ts
@@ -0,0 +1,45 @@
+
+export const ewdCourses = [
+    {
+      title: "Full Stack Foundations",
+      href: "/posts/full-stack-foundations",
+    },
+    {
+      title: "Web Form",
+      href: "/posts/web-form",
+    },
+    {
+      title: "Data Modeling",
+      href: "/posts/data-modeling",
+    },
+  ]
+  
+  export const femCourses = [
+    {
+      title: "TypeScript Path",
+      url: "https://www.typescript-training.com/course/fundamentals-v4",
+    },
+    {
+      title: "Vite Workshop",
+      url: "https://vite-workshop.vercel.app/introduction",
+    },
+    {
+      title: "Basics of Go",
+      url: "https://frontendmasters.com/courses/go-basics/",
+    },
+  ]
+  
+  export const eggCourses = [
+    {
+      title: "Script Kit Showcase",
+      url: "https://www.scriptkit.com/tutorials/automation-workflows-for-web-developers/scriptkit-showcase/scripting-hacker-news",
+    },
+    {
+      title: "React Testing Library",
+      url: "https://egghead.io/courses/an-introduction-to-the-react-testing-library-42204aaa?af=8mci9b",
+    },
+    {
+      title: "Mock Service Worker",
+      url: "https://egghead.io/courses/mock-rest-and-graphql-apis-with-mock-service-worker-8d471ece",
+    },
+  ]
