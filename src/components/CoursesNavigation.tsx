159fead6d856cf72ef40db4d873bf7a7d2b52e87
ROSORRNT <45917975+ROSORRNT@users.noreply.github.com>
2024-02-19 13:10:43 +0100

    Refactor course navigation links

--- src/components/CoursesNavigation.tsx
+++ src/components/CoursesNavigation.tsx
@@ -26,14 +26,21 @@ export default function CoursesNavigation({ courses, title }: { courses: Course[
           <NavigationMenuContent>
 
             {courses.map((course) => (
-              <NavigationMenuItem className="hover:bg-chinook-50
+              <NavigationMenuItem key={course.title} className="hover:bg-chinook-50
                px-4
                py-2
                block
              ">
-                <a href={course.href !== undefined ? course.href : course.url} target="_blank" rel="noopener noreferrer" >
-                  {course.title}
-                </a>
+                {
+                  course.href !== undefined ? (
+                    <NavigationMenuLink href={course.href}>{course.title}</NavigationMenuLink>
+                  ) : (
+                    <a href={course.url} target="_blank" rel="noopener noreferrer" >
+                      {course.title}
+                    </a>
+                  )
+                }
+
               </NavigationMenuItem>
             ))}
 
