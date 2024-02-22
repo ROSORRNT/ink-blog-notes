360772e37e250fbf9c6df2a21a3b98a1b86983e0
ROSORRNT <45917975+ROSORRNT@users.noreply.github.com>
2024-02-22 12:16:21 +0100

    Update CoursesNavigation component

--- src/components/CoursesNavigation.tsx
+++ src/components/CoursesNavigation.tsx
@@ -27,12 +27,12 @@ export default function CoursesNavigation({ courses, title }: { courses: Course[
           <NavigationMenuContent>
 
             {courses.map((course) => (
-              <NavigationMenuItem key={course.title} className="hover:bg-chinook-50 px-4 py-2 block">
+              <NavigationMenuItem key={course.title} className="hover:bg-chinook-50 px-4 py-2 block text-left ">
                 {
                   course.href !== undefined ? (
-                    <NavigationMenuLink href={course.href}>{course.title}</NavigationMenuLink>
+                    <NavigationMenuLink className="no-underline" href={course.href}>{course.title}</NavigationMenuLink>
                   ) : (
-                    <a href={course.url} target="_blank" rel="noopener noreferrer" >
+                    <a className="no-underline" href={course.url} target="_blank" rel="noopener noreferrer" >
                       {course.title}
                     </a>
                   )
